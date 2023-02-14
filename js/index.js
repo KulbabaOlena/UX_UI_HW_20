bbeafe6dc7b3cbb20a9fbab1680f36417beac89b
KulbabaOlena <110860796+KulbabaOlena@users.noreply.github.com>
2023-02-14 16:14:13 -0500

    piu

--- js/index.js
+++ js/index.js
@@ -19,3 +19,33 @@ function buttonMouseExit(eleId)
 	btn.style.background = "LINEAR-GRADIENT(RGB(254, 103, 191), RGB(254, 205, 238))";
 }
 
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
+function scrollFunction1() {
+	let e = document.getElementById("el1");
+	e.scrollIntoView({
+	  block: 'start',
+	  behavior: 'smooth',
+	  inline: 'start'
+	});
+  }
+  
